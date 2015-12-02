f1d43ec9ce9692b0cd70b19eb5c7e8ca4bcafed4
Joel Cormack <joel@robottoilets.co.uk>
2015-12-02 13:42:51 +0000

    added installation, site and contact models

--- woowooapp.py
+++ woowooapp.py
@@ -6,6 +6,37 @@ from google.appengine.ext import ndb
 from webapp2_extras import json
 import webapp2
 
+class Site(ndb.Model):
+    """Models a site with a site name, closing date, address"""
+    name = ndb.StringProperty()
+    location = ndb.GeoPtProperty()
+    address_one = ndb.StringProperty()
+    address_two = ndb.StringProperty()
+    postcode = ndb.StringProperty()
+
+class Contact(ndb.Model):
+    """Models a contact of the site with a name, email and phone numbers"""
+    name = ndb.StringProperty()
+    email = ndb.StringProperty()
+    phone = ndb.IntegerProperty()
+    mobile = ndb.IntegerProperty()
+
+class Installation(ndb.Model):
+    """Models an installation with a date set on creation, installation date,
+    delivery date and pickup date, booleans to monitor stages of confirmations for
+    contractot, haulier, customer and retailer and associated sites and contacts.
+    """
+    created_date = ndb.DateTimeProperty(auto_now_add=True)
+    installation_date = ndb.DateTimeProperty()
+    delivery_date = ndb.DateTimeProperty()
+    pickup_date = ndb.DateTimeProperty()
+    contractor_confirmed = ndb.BooleanProperty(default=False)
+    haulier_confirmed = ndb.BooleanProperty(default=False)
+    customer_confirmed = ndb.BooleanProperty(default=False)
+    retailer_confirmed = ndb.BooleanProperty(default=False)
+    sites = ndb.StructuredProperty(Site, repeated=True)
+    contacts = ndb.StructuredProperty(Contact, repeated=True)
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.write('hello')
@@ -22,8 +53,18 @@ class Start(webapp2.RequestHandler):
         parsed_json = json.decode(data)
         site = parsed_json['site']
         contact = parsed_json['contact']
-        print site
-        print contact
+        installation = Installation(
+                sites=[Site(name = parsed_json['site']['name'],
+                            address_one = parsed_json['site']['address one'],
+                            address_two = parsed_json['site']['address two'],
+                            postcode = parsed_json['site']['postcode'])],
+                contacts=[Contact(name=parsed_json['contact']['name'],
+                            email = parsed_json['contact']['email'],
+                            phone = int(parsed_json['contact']['phone']),
+                            mobile = int(parsed_json['contact']['mobile']))]
+        )
+        installation_key = installation.put()
+        print installation_key
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
